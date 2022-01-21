37abf0ec9712071743c0b8aac0268f6041355544
rajeevsujakhu <98156872+rajeevsujakhu@users.noreply.github.com>
2022-01-21 16:15:22 +0545

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $(".first").owlCarousel({
+        items: 2,
+        loop: true,
+        margin: 35,
+        nav: true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    $('.second').owlCarousel({
+        loop:true,
+        margin:15,
+        
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+    $(document).ready(function(){
+        $(".third").owlCarousel({
+            items:6,
+            loop:true,
+            margin:10,
+        });
+      });
+});
+$(window).scroll(function(){
+    if($(this).scrollTop()>100)
+    {
+      $("header").addClass("stickyy") 
+      $(".arrow").show()
+    }
+    else
+    {
+        $("header").removeClass("stickyy")
+        $(".arrow").hide()
+    }
+})
